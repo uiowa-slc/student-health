cd33af83cec455aa96d9dd4d362d4ca338402b4d
Dustin Quam <quamsta@gmail.com>
2012-09-12 10:43:57 -0500

    drupal theme commit (for reference)

---
+++ themes/shs_drupal/block-block-2.tpl.php
@@ -0,0 +1,20 @@
+
+<div id="block-1">
+
+<div id="search">
+<form method="get" action="http://search.uiowa.edu/search">
+<fieldset><input type="text" name="q" value="" title="text" ></input> 
+<input type="submit" name="btnG" value="Search" class="submit"  ></input> 
+<input type="hidden" name="site" value="default_collection" ></input> 
+<input type="hidden" name="client" value="default_frontend"  ></input> 
+<input type="hidden" name="output" value="xml_no_dtd"  ></input> 
+<input type="hidden" name="proxystylesheet" value="default_frontend" ></input> 
+<input type="hidden" name="sitesearch" value="studenthealth.uiowa.edu"  ></input></fieldset> </form>
+</div>
+
+
+  <?php print $edit_links; ?>
+
+</div> <!-- /block-inner, /block -->
+
+
