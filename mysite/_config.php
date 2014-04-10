0267f3ca2b27dc57f0179433b4141e29ce30e27a
Dustin Quam <quamsta@gmail.com>
2014-04-10 12:37:42 -0500

    removed old drupal stuff, renamed theme so composer wouldn't override

---
+++ mysite/_config.php
@@ -0,0 +1,29 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'studenthealth_silverstripe';
+ 
+// Use _ss_environment.php file for configuration
+require_once("conf/ConfigureFromEnv.php");
+
+MySQLDatabase::set_connection_charset('utf8');
+
+// This line set's the current theme. More themes can be
+// downloaded from http://www.silverstripe.org/themes/
+SSViewer::set_theme('scope');
+// Set the site locale
+i18n::set_locale('en_US');
+
+// enable nested URLs for this site (e.g. page/sub-page/)
+SiteTree::enable_nested_urls();
+/* Paste as Plain Text By Default */
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky',true);
+HtmlEditorConfig::get('cms')->setOption('paste_text_sticky_default',true);
+
+SpamProtectorManager::set_spam_protector("RecaptchaProtector");
+
+RecaptchaField::$public_api_key = '6LcjsAgAAAAAAD6MXE7QNLusIBMajgpfK_EWjL3C';
+RecaptchaField::$private_api_key = '6LcjsAgAAAAAAJNAXp7BiEvEas6scIeWzQdaS87c';
