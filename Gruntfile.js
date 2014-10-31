aa2b6dfe4842db1b1c3a7298d67a429c40808309
Jonathan Solis <athaax@gmail.com>
2014-10-31 13:16:41 -0500

    Division bar updated, grunt file update

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
         },                  // Target
         options: {              // Target options
           style: 'compressed',
-          sourcemap: 'true',
+          //sourcemap: 'true',
           loadPath: ['division-project/scss']
         }
       }
