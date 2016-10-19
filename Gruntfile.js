4c7efd07059eb9677c9e59d032aa8508485a69ee
Dustin Quam <dustinquam@me.com>
2016-10-19 13:34:58 -0500

    updates to dp

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,6 +104,6 @@ module.exports = function(grunt) {
 
   // Default task(s).
   // Note: order of tasks is very important
-  grunt.registerTask('default', ['sass', 'concat', 'uglify', 'criticalcss', 'cssmin','watch']);
+  grunt.registerTask('default', ['sass', 'concat', 'uglify', 'cssmin','watch']);
 
 };
